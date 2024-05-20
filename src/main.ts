efdbc03ea7b9049611618f2cd238587f6c691e67
RodolfoMarinero <rmarinero50@gmail.com>
2024-05-20 16:39:36 -0600

    primer commit

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { InicioComponent } from './app/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
+  imports: [InicioComponent],
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+  <app-inicio></app-inicio>
   `,
 })
 export class App {
