3e5a8450fe6396de8816bd11e489f8d1e3db1b83
RodolfoMarinero <rmarinero50@gmail.com>
2024-05-20 17:46:56 -0600

    :3

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { IniciarSesionComponent } from './app/iniciar-sesion/iniciar-sesion.component';
 import { InicioComponent } from './app/inicio/inicio.component';
+import { HomeComponent } from './app/home/home.component';
 
 export const routes: Routes = [   
     { path: '', component: InicioComponent, pathMatch: 'full' },  
     { path: 'inicioSesion', component: IniciarSesionComponent },
-   
+    { path: 'home', component: HomeComponent },
 ];
--- src/app/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-iniciar-sesion',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
