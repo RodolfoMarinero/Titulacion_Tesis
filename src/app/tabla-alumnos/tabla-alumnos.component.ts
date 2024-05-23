b7c3c8ed1d310688f8dcfa8eae1eb64eb3b99c82
RodolfoMarinero <rmarinero50@gmail.com>
2024-05-23 13:57:18 -0600

    revisar

--- src/app/tabla-alumnos/tabla-alumnos.component.ts
+++ src/app/tabla-alumnos/tabla-alumnos.component.ts
@@ -7,10 +7,14 @@ import {
   Validator,
   Validators,
 } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Tesista } from '../../model/tesista';
+import { ListaTesistas } from '../../model/listaTesistas';
+import { ConexionBDService } from '../conexion-bd.service';
 @Component({
   selector: 'app-tabla-alumnos',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,RouterModule],
   templateUrl: './tabla-alumnos.component.html',
   styleUrl: './tabla-alumnos.component.css',
 })
@@ -18,7 +22,10 @@ export class TablaAlumnosComponent {
   public listaUsers: ListaTesistas = new ListaTesistas();
   @Output() flag: EventEmitter<boolean> = new EventEmitter<boolean>();
   //@Output() idDet: EventEmitter<number> = new EventEmitter();
-  constructor() {}
+  constructor(public service:ConexionBDService) {
+    this.listaUsers=service.getTesistas();
+  }
+
   //public agregar() {
   //const id = parseInt(this.frmLista.get('id')?.value);
   //const name = this.frmLista.get('name')?.value;
@@ -34,67 +41,5 @@ export class TablaAlumnosComponent {
 
   public verDetalle(id: number) {
     this.flag.emit(true); // Cambia el valor que deseas emitir aquí
-  }
-}
-class Tesista {
-  public id!: number;
-  public nombre: String = '';
-  public tesis: string = '';
-  public carrera: string = '';
-  public notificacion: boolean = false;
-
-  constructor(
-    id: number,
-    nombre: String,
-    tesis: string,
-    carrera: string,
-    notificaion: boolean
-  ) {
-    this.id = id;
-    this.nombre = nombre;
-    this.tesis = tesis;
-    this.carrera = carrera;
-  }
-}
-
-class ListaTesistas {
-  public users: Tesista[] = [];
-
-  constructor() {
-    this.get();
-  }
-
-  //public agregar(user: Tesista) {
-  //this.users.push(user);
-  //this.save();
-  //this.get();
-  //}
-
-  //public remove(index: number) {
-  //this.users.splice(index, 1);
-  //this.save();
-  //}
-
-  //public save() {
-  //localStorage.setItem('users', JSON.stringify(this.users));
-  //}
-
-  public get(): void {
-    const userFromLocal = JSON.parse(localStorage.getItem('users') || '[]');
-    this.users.splice(0);
-    if (Array.isArray(userFromLocal)) {
-      this.users = userFromLocal.map(
-        (user: any) =>
-          new Tesista(
-            user.id,
-            user.nombre,
-            user.tesis,
-            user.carrera,
-            user.notificacion
-          )
-      );
-    } else {
-      console.error('El valor recuperado de localStorage no es un array.');
-    }
   }
 }
--- src/model/listaTesistas.ts
+++ src/model/listaTesistas.ts
@@ -1,30 +1,28 @@
 import { ConexionBDService } from "../app/conexion-bd.service";
 import { Tesista } from "./tesista";
 
-class ListaTesistas {
+export class ListaTesistas {
     public tesistas: Tesista[] = [];
 
-    constructor(public service:ConexionBDService) {
-        this.get();
+    constructor() {
+       
 
     }
 
-    public agregar(user: Tesista) {
-        this.tesistas.push(user);
-        this.save();
-        this.get();
-    }
+   // public agregar(user: Tesista) {
+     //   this.tesistas.push(user);
+        //this.save();
+    //    this.get();
+   // }
 
-    public remove(index: number) {
-        this.tesistas.splice(index, 1);
-        this.save(this.tesistas);
-    }
+   // public remove(index: number) {
+    //    this.tesistas.splice(index, 1);
+    //    this.save(this.tesistas);
+    //}
 
-    public save(tesistas:Tesista[]) {
-        localStorage.setItem('users', JSON.stringify(tesistas));
-    }
+    //public save(tesistas:Tesista[]) {
+    //    localStorage.setItem('users', JSON.stringify(tesistas));
+   // }
 
-    public get(): void {
-        this.service.getTesistas();
-    }
+    
 }
