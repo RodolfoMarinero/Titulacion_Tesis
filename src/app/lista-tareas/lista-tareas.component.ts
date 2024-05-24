8819772d41a498e8127c018ec91fc37b7f10e9b8
CarlosRxD <cardozacarlosr@gmail.com>
2024-05-23 22:32:32 -0600

    Update Progreso

---
+++ src/app/lista-tareas/lista-tareas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-tareas',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-tareas.component.html',
+  styleUrl: './lista-tareas.component.css'
+})
+export class ListaTareasComponent {
+
+}
