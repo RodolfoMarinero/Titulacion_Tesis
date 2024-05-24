f6fee39704bbb882b382e76691368b05e8204839
RodolfoMarinero <rmarinero50@gmail.com>
2024-05-24 13:04:31 -0600

    gnsrdgnj

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './app/home/home.component';
 import { VistaRevisarTesisComponent } from './app/vista-revisar-tesis/vista-revisar-tesis.component';
 import { TablaAlumnosComponent } from './app/tabla-alumnos/tabla-alumnos.component';
 import { RegistrarTesistaComponent } from './app/registrar-tesista/registrar-tesista.component';
+import { AsignarRevisoresComponent } from './app/asignar-revisores/asignar-revisores.component';
 
 export const routes: Routes = [   
     { path: '', component: InicioComponent, pathMatch: 'full' },  
@@ -12,4 +13,5 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'revisarTesis', component: TablaAlumnosComponent },
     { path: 'registrarTesista', component: RegistrarTesistaComponent },
+    { path: 'asignarRevisores', component: AsignarRevisoresComponent },
 ];
---
+++ src/app/asignar-revisores/asignar-revisores.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-asignar-revisores',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './asignar-revisores.component.html',
+  styleUrl: './asignar-revisores.component.css'
+})
+export class AsignarRevisoresComponent {
+
+}
