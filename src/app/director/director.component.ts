be9ba1414fddfc4cbea28fa43ea46f3883d0eba6
RodolfoMarinero <rmarinero50@gmail.com>
2024-05-29 23:17:34 -0600

    menu

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TablaAlumnosComponent } from '../tabla-alumnos/tabla-alumnos.component';
+import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-director',
   standalone: true,
-  imports: [],
+  imports: [TablaAlumnosComponent, NavMenuComponent],
   templateUrl: './director.component.html',
   styleUrl: './director.component.css'
 })
